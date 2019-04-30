f0e8a2ac0c71e9c4dbe5ded7af7a152ba4d135e2
Jean Abayo <abagammy@gmail.com>
2019-04-30 11:16:06 +0300

    Define routes and controllers

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const {
+	fetchLocations,
+	createNewLocation,
+	fetchNestedLocation,
+	createNestedLocation,
+	updateLocationDetails,
+	deleteLocation,
+} = require('./controller')
 
-app.get('/', (req, res) => res.status(200).json('Welcome to populatio 😄'));
-
-app.post('/')
+app.get('/', fetchLocations);
+app.post('/', createNewLocation);
+app.get('/:location-name', fetchNestedLocation);
+app.post('/:location-name', createNestedLocation);
+app.put('/:location-name', updateLocationDetails);
+app.delete('/:location-name', deleteLocation);
 
 app.listen(port, () => console.log(`App running on port ${port}!`))
---
+++ controller.js
@@ -0,0 +1,43 @@
+// GET          Fetch all locations
+// GET          Fetch by genre
+exports.fetchLocations = (req, res) => {
+	res.status(200).json({
+		message: 'Locations retrieved successfuly'
+	})
+}
+
+// POST         Create a new location
+exports.createNewLocation = (req, res) => {
+	res.status(201).json({
+		message: 'Location created successfuly'
+	})
+}
+
+// GET          Fetch single location data
+// GET          Fetch location data by genre
+exports.fetchNestedLocation = (req, res) => {
+	res.status(200).json({
+		message: 'Locations retrieved successfuly'
+	})
+}
+
+// POST         Create location under location
+exports.createNestedLocation = (req, res) => {
+	res.status(201).json({
+		message: 'Location created successfuly'
+	})
+}
+
+// PUT          Update location details
+exports.updateLocationDetails = (req, res) => {
+	res.status(200).json({
+		message: 'Location updated successfuly'
+	})
+}
+
+// DELETE       Delete location and it's details
+exports.deleteLocation = (req, res) => {
+	res.status(200).json({
+		message: 'Location deleted successfuly'
+	})
+}
